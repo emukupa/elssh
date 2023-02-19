00af979d93bd7cb36d6ef348aa3edf3034bde88a
Edward Manda <lilkups@yahoo.com>
2023-02-19 14:52:56 -0600

    execute framework implemented

---
+++ elssh-execute.c
@@ -0,0 +1,10 @@
+#include "elssh.h"
+
+/**
+ * @brief execute the command line tokens
+ * @param tokens command line array pointer of tokens 
+ * @return the status of the commands
+*/
+extern int elssh_execute(char **tokens){
+  return 1;
+}
