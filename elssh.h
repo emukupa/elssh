26d51ef3096aaf9a1f6280660af009306a619ef7
Edward Manda <lilkups@yahoo.com>
2023-02-19 12:02:26 -0600

    define the basic functions needed

--- elssh.h
+++ elssh.h
@@ -3,4 +3,22 @@
 
 #include <stdio.h>
 
+/**
+ * @brief Initialize the environment
+ * @return void
+*/
+extern void elssh_init();
+
+/**
+ * @brief Run the shell
+ * @return void
+*/
+extern void elssh_run();
+
+/**
+ * @brief Clean up the environment
+ * @return void
+*/
+extern void elssh_cleanup();
+
 #endif // ELSSH_H
--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "elssh.h"
 
 int main(){
-    printf("Hello World!\n");
+    // Initialize the environment
+    // elssh_init();
+
+    // continuously run the shell until stopped
+    // elssh_run();
+
+    // clean up the environment
+    // elssh_cleanup();
 
     return 0;
 }
